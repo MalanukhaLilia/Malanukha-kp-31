c0869914d2003fe330230ecb7edb7eab515e3ec9
MalanukhaLilia <148195078+MalanukhaLilia@users.noreply.github.com>
2024-05-14 18:53:56 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Malanukha kp-31',
   tagline: 'I like cats',
-  favicon: 'docs/img/favicon.ico',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
   url: 'https://MalanukhaLilia.github.io',
@@ -69,7 +69,7 @@ const config = {
         title: 'My Site',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'docs/img/logo.svg',
         },
         items: [
           {
